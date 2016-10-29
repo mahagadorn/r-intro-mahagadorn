0b69268980c31b52ccaa01f620253f9ed283925f
mahagadorn <mahagadorn@aggiemail.usu.edu>
2016-10-28 20:39:02 -0600

    lesson_2.R

--- lesson_2.R
+++ lesson_2.R
@@ -439,8 +439,8 @@ ImABox <- function(width,height){
   ast.vect <- c("*")
   cat(noquote(paste(rep(ast.vect, times = width), collapse = ""))) #saying repeat asteric vector the same number of times as width
   cat("\n")     #saying to go to the next line, if you don't do this it won't move to the middle section
-    for (i in height){
-      cat(ast.vect, paste(rep(width-2), collapse = ""), ast.vect) #something is wrong here
+    for (i in width){
+      cat(ast.vect, paste(rep(" ", times=(1/2*(width-2)))), ast.vect) #something is wrong here
   }
   cat("\n") 
   cat(noquote(paste(rep(ast.vect, times = width), collapse = ""))) #last line
@@ -448,12 +448,64 @@ ImABox <- function(width,height){
 
 ImABox(5,3)
 
+# 10. Modify your box function to put text centred inside the box, like this:
+  # *************
+  # *           *
+  # * some text *
+  # *           *
+  # *************
+
+#okay so list things we need to do.
+#we need to take into consideration how we can to add text
+      #should be able to do this by simply removing the rep function and using only paste???
+#also need to figure out how to make sure the text isn't to large for the box?
+      #nchar() counts the number of characters in a vector
+      #maybe we can do value of width minus number of characters in the text?
+      #nchar(text) > width-2 This should be a true or fa1se statement. Would make it okay for an if loop
+      #if else would allow us to say if it is greater than this STOP but if not then keep going
+      
+
+TextBox <- function(width, height, text){
+  #Define our vectors....Reminder, you had this built into your loops and they became WAY to chunky! so you removed them and placed them up here
+  ast.vect <- c("*")
+  roomLR <- (((width-2)-nchar(text))/2) #I think this will count the number of characters???
+  RMLines <- height-3
+  Top <- RMLines/2
+  Bottom <- RMLines/2
+
+  
+   #saying repeat asteric vector the same number of times as width    #saying to go to the next line, if you don't do this it won't move to the middle section
+  for (i in width){
+      if(nchar(text) > width-4) {
+        print("Long winded? Your text has too many characters!")
+      } else{
+        #making vectors
+        space <- c(ast.vect, paste(rep("", times=(width-3))), ast.vect, "\n")
+        Lside <- rep("", times= (((width-3)-nchar(text))/2))
+        Rside <- rep("", times= (((width-3)-nchar(text))/2))
+        textline <- c(ast.vect, Lside, text, Rside, ast.vect, "\n")
+        #Top Line of Astericks  
+        cat((noquote(paste(rep(ast.vect, times = width), collapse = ""))), "\n")
+          #Start loop for spaced
+          for(i in 1:Top){
+            cat(space)
+          }
+            cat(textline)
+          #Another loop for Space
+          for(i in 1:Bottom){
+            cat(space)
+          }
+        #Bottom Line of Astericks  
+        cat((noquote(paste(rep(ast.vect, times = width), collapse = ""))), "\n")
+    }
+  }
+}
 
 
+TextBox(23, 13, text="Keep Calm, Code On!")
 
 
-
-
+#11
 
 
 
