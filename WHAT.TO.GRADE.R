3a835c486615cb46cd5e432d381897d017221419
mahagadorn <mahagadorn@aggiemail.usu.edu>
2016-11-04 16:45:28 -0600

    WHAT.TO.GRADE.R

---
+++ WHAT.TO.GRADE.R
@@ -0,0 +1,16 @@
+#The following is what I would like graded
+
+#Lesson One: All
+
+#Lesson Two: 1-15  --15 total
+
+#Lesson Three: 1-11   --15+11=26
+
+#Lesson Four: 1-3 & 5  --26+4=30
+
+
+#There are plent of unfinished ones...Feel free to peruse those if you would like.
+
+
+#Thanks for all of you help! Onward we march towards building our own worlds :)
+#I think I have officially upgraded to Iron Man's cousin...the one who steals his armor and pretends to be a super hero :)
